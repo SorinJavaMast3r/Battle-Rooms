9ad5312e62cacd839a7576ac99d411741109cd4f
SorinJavaMast3r <45288233+SorinJavaMast3r@users.noreply.github.com>
2020-11-14 12:14:29 +0100

    Desarrollo Menu Principal
    Hemos migrado los archivos de un antiguo repositorio al actual porque hemos cambiado el proyecto de unity a un nuevo formato

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class Loading : MonoBehaviour
+{
+
+    VideoPlayer videoPlayer;
+    public string escenaACargar;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+    
+    void Update()
+    {
+        if ((videoPlayer.frame) > 0 && (videoPlayer.isPlaying == false))
+        {
+            SceneManager.LoadScene(escenaACargar);
+        }
+    }
+}
