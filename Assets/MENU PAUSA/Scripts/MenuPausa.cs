7c9ae7680c321a1c4e2f6be9852dc2642c463332
AdrianPerezDiaz <adri672perez@gmail.com>
2020-11-22 18:31:13 +0100

    Creacción del menú

---
+++ Assets/MENU PAUSA/Scripts/MenuPausa.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //Para poder gestionar diferentes escenas
+public class MenuPausa : MonoBehaviour
+{
+    public void Jugar()
+    {
+        
+
+    }
+
+    public void Salir()
+    {
+        SceneManager.LoadScene("Escena menu principal"); //Me va a cargar la escena del juego
+    }
+}
