e2d36fdfa2ff4dcb4d7b6d6252dd8cce373c78c4
SokerOne <45288150+SokerOne@users.noreply.github.com>
2020-12-23 21:13:08 +0100

    [IMPROVE] Warrior jerarchy animations.
    animator, animator controller and other adjustments in the warrior animations.

---
+++ Assets/Resources/Scripts/playerBehavieur.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerBehavieur : MonoBehaviour
+{
+    public float movementSpeed = 5.0f;
+    public float rotationSpeed = 200.0f;
+    public Animator anim;
+    public float x, y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+    }
+}
