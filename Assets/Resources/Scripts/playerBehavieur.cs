d6a15b94bb555191676bc1bf3388573e782a23d5
AdrianPerezDiaz <adri672perez@gmail.com>
2020-12-28 16:42:16 +0100

    [ADD] Animations
    Añadir animacion.
    Implementar esas animaciones.
    Crear AnimationController

--- Assets/Resources/Scripts/playerBehavieur.cs
+++ Assets/Resources/Scripts/playerBehavieur.cs
@@ -9,9 +9,14 @@ public class playerBehavieur : MonoBehaviour
     public Animator anim;
     public float x, y;
 
+    public Rigidbody rb;
+    public float salto = 8f;
+    public bool puedoSaltar;
+
     // Start is called before the first frame update
     void Start()
     {
+        puedoSaltar = false;
         anim = GetComponent<Animator>();
     }
 
@@ -20,5 +25,36 @@ public class playerBehavieur : MonoBehaviour
     {
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
+
+        anim.SetFloat("Horizontal", x);
+        anim.SetFloat("Vertical", y);
+
+        if (puedoSaltar)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                anim.SetBool("saltar", true);
+                rb.AddForce(new Vector3(0, salto, 0), ForceMode.Impulse);
+            }
+            anim.SetBool("suelo", true);
+        }
+        else
+        {
+            EstoyCayendo();
+        }
+
+        
+    }
+
+    public void EstoyCayendo()
+    {
+        anim.SetBool("suelo", false);
+        anim.SetBool("saltar", false);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, x * Time.deltaTime * rotationSpeed, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * movementSpeed);
     }
 }
