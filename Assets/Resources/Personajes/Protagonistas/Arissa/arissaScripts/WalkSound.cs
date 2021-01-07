8cee0081c97ef53154f9664ca290b1c0ace1120c
SokerOne <45288150+SokerOne@users.noreply.github.com>
2021-01-07 16:01:41 +0100

    [ADD] Single FootSteep Sound added.

---
+++ Assets/Resources/Personajes/Protagonistas/Arissa/arissaScripts/WalkSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkSound : MonoBehaviour
+{
+    public AudioSource foot;
+
+	private void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Terrain")
+		{
+			foot.Play();
+		}
+	}
+}
