a6a98517c5e13750d36f8b2b09fa78aa6fcb70cf
SokerOne <45288150+SokerOne@users.noreply.github.com>
2021-01-07 16:52:52 +0100

    [IMPROVE] PantallaDeLogoJuego builded.

--- Assets/Resources/Personajes/Protagonistas/Arissa/arissaScripts/WalkSound.cs
+++ Assets/Resources/Personajes/Protagonistas/Arissa/arissaScripts/WalkSound.cs
@@ -6,12 +6,15 @@ public class WalkSound : MonoBehaviour
 {
     public AudioSource foot;
 
+	[Range(0.00f, 1.00f)]
+	public float vol;
+
 	private void OnTriggerEnter(Collider col)
 	{
 		if (col.gameObject.tag == "Terrain")
 		{
 			foot.Play();
-			foot.volume = 0.05f;
+			foot.volume = vol;
 		}
 	}
 }
