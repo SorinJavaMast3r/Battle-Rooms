cc68d4c401a7351f6948dc8e0207db05b8f92f24
SorinJavaMast3r <45288233+SorinJavaMast3r@users.noreply.github.com>
2021-01-10 21:28:52 +0100

    [ADD] Implementacion de IA
    He añadido y empezado a configurar unos assets utiles para desarrollar la IA

--- Assets/Resources/Personajes/Enemigos/Scripts/IA.cs
+++ Assets/Resources/Personajes/Enemigos/Scripts/IA.cs
@@ -6,113 +6,225 @@ using UnityEngine.AI;
 public class IA : MonoBehaviour
 {
 
-    [Header("Ajustes")]
-    public int tiempo;
-    public float velocidad;
-    public Transform puntoGuarida;
-    public NavMeshAgent agent;
-    public Animator anim;
+    private Animator animator;
 
-    [Header("Estados")]
-    public bool idle;
-    public bool atacar;
-    public bool asustado;
-    public int estado = 1;
+    private NavMeshAgent nav;
 
-    bool cambio = true;
-    bool tiempodegiro;
-    float y;
-    Vector3 player;
+    private SphereCollider col;
+
+    private GameObject player;
+
+
+    public float speed = 0.0f;
+    public float h = 0.0f, v = 0.0f;
+    public bool attack = false, //Está atacando?
+                die = false; //Está muerto?
+
+    public bool DEBUG = false, DEBUG_DRAW = false;
+
+    public Vector3 direction; //Donde está player en relación a NPC
+
+    public float distance = 0.0f; //Distancia entre jugador y NPC
+
+    public float angle = 0.0f; //Ángulo entre jugador y NPC
+
+    public bool playerInSight = false; //Está el jugador en el FOV del NPC?
+
+    public float fieldOfViewAngle = 120;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+        col = GetComponent<SphereCollider>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (die)
+        {
+            //TODO: animar la muerte del NPC
+            //- poner a true un parámetro die como el del personaje
+            //- desactivar los scripts de movimiento del NPC
+            //- Preprogramar la destrucción del NPC dentro de 3 segundos.
+            Destroy(gameObject);
+        }
+
+        if (playerInSight)
+        {
+            this.transform.rotation = //Girar al NPC 
+                Quaternion.Slerp(this.transform.rotation, //desde donde mira ahora
+                                 Quaternion.LookRotation(direction), //hacia la dirección del player
+                                 0.1f //en un tiempo de 0.1f
+                                );
+        }
+
+        //if (player.transform.GetComponent<DragonCharacterController>() &&
+        //    player.transform.GetComponent<DragonCharacterController>().dead)
+        //{
+        //    animator.SetBool("attack", false);
+        //    animator.SetFloat("speed", 0);
+        //    animator.SetFloat("angularSpeed", 0);
+        //}
+
+
+    }
 
     private void FixedUpdate()
     {
-        tiempo += 1;
-
-        if(estado == 1)
+        h = angle;
+        v = distance;
+        speed = distance / Time.deltaTime;
+        if (DEBUG)
         {
-            idle = true;
-            atacar = false;
-            asustado = false;
-        } else if (estado == 2)
-        {
-            idle = false;
-            atacar = true;
-            asustado = false;
-        } else if (estado == 3)
-        {
-            idle = false;
-            atacar = false;
-            asustado = true;
+            Debug.Log(string.Format("H:{0} - V:{1}, S:{2}", h, v, speed));
         }
+        animator.SetFloat("speed", speed);
+        animator.SetFloat("angularSpeed", h);
+        animator.SetBool("attack", attack);
 
-        if (idle)
+
+        //if (playerInSight)
+        //{
+        //    if (animator.GetFloat("attack2") > 0.5f || animator.GetFloat("attack3") > 0.5f)
+        //    {
+        //        if (player.GetComponent<PlayerAgent>() != null)
+        //        {
+        //            float baseDamage = 10.0f;
+        //            float enemyAttack = GetComponent<NPCAgent>().npcData.strength;
+        //            float playerDefense = player.GetComponent<PlayerAgent>().playerCharacterData.defense;
+        //            //TODO: si hay armas equipadas o armaduras lo suyo es hacer un bucle e incrementar 
+        //            //el ataque/defensa según los calificadores de las armas o armaduras en cuestión
+        //            player.GetComponent<PlayerAgent>().playerCharacterData.health -=
+        //                baseDamage * enemyAttack / playerDefense * Time.deltaTime;
+        //        }
+        //    }
+        //}
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        //if (player.GetComponent<DragonCharacterController>() &&
+        //   player.GetComponent<DragonCharacterController>().dead)
+        //{
+        //    return;
+        //}
+
+        if (other.transform.tag.Equals("Player"))
         {
-            transform.Translate(Vector3.forward * velocidad * Time.fixedDeltaTime);
-            transform.Rotate(new Vector3(0, y, 0));
-            anim.SetBool("Caminar", true);
-            anim.SetBool("Correr", false);
-            if (tiempo >= Random.Range(100, 2500))
+            Debug.Log("ENTRANDO EN EL TRIGGER");
+            //vector = destino - origen
+            direction = player.transform.position - this.transform.position;
+            distance = Vector3.Magnitude(direction);
+            angle = Vector3.Dot(this.transform.forward, player.transform.position);
+
+            if (DEBUG_DRAW)
             {
-                Girar();
-                tiempo = 0;
-                tiempodegiro = true;
+                Debug.DrawLine(this.transform.position + Vector3.up,
+                               direction * 50, Color.red);
+                Debug.DrawLine(player.transform.position,
+                               this.transform.position,
+                               Color.blue);
             }
 
-            if (tiempodegiro)
+
+            playerInSight = false;
+            float calculateAngle = Vector3.Angle(direction, transform.forward);
+            //Si el player está en el campo de visión
+            if (calculateAngle < 0.5f * fieldOfViewAngle)
             {
-                if (tiempo > Random.Range(10, 30))
+                RaycastHit hit;
+                if (DEBUG_DRAW)
                 {
-                    y = 0;
-                    tiempodegiro = false;
+                    Debug.DrawRay(this.transform.position + transform.up,
+                                  direction.normalized, Color.green);
+                }
+                //Trazo un rayo desde NPC hasta player
+                if (Physics.Raycast(transform.position + transform.up, direction.normalized,
+                                   out hit, col.radius))
+                {
+                    //Si lo primero que localiza el rayo es el jugador
+                    if (hit.collider.gameObject == player)
+                    {
+                        playerInSight = true;
+                        if (DEBUG)
+                        {
+                            Debug.Log("Jugador en el campo de visión!!!!");
+                        }
+                    }
                 }
             }
-        } else if (atacar)
-        {
-            anim.SetBool("Caminar", false);
-            anim.SetBool("Correr", true);
-            player = GameObject.FindGameObjectWithTag("Player").transform.position;
-            agent.SetDestination(player);
-            if(Vector3.Distance(player, transform.position) > 30)
+
+            //Si después de toda la comprobación anterior, el player está en FoV del NPC
+            if (playerInSight)
             {
-                estado = 1;
-                cambio = true;
-            }
-        } else if (asustado)
-        {
-            anim.SetBool("Caminar", false);
-            anim.SetBool("Correr", true);
-            agent.SetDestination(puntoGuarida.transform.position);
-            if (Vector3.Distance(puntoGuarida.position, transform.position) < 3)
-            {
-                anim.SetBool("Caminar", false);
-                anim.SetBool("Correr", false);
-                if (tiempo > Random.Range(500, 1000))
+                nav.SetDestination(player.transform.position);
+                CalculatePathLength(player.transform.position);
+                //Si estoy muy cerca, puedo atacar...
+                if (distance < 1.1f)
                 {
-                    estado = 1;
+                    attack = true;
+                }
+                else
+                {
+                    attack = false;
                 }
             }
+
+
         }
     }
 
-    public void Girar()
+    private void OnTriggerExit(Collider other)
     {
-        y = Random.Range(-3, 3);
-    }
-
-    public void CambiarEstado()
-    {
-        estado = Random.Range(1, 3);
-        if(estado == 2)
+        if (other.tag == "Player")
         {
-            cambio = false;
+            distance = 0;
+            angle = 0;
+            attack = false;
+            playerInSight = false;
+            direction = Vector3.zero;
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    float CalculatePathLength(Vector3 targetPosition)
     {
-        if(other.tag == "Player" && cambio)
+
+        NavMeshPath path = new NavMeshPath();
+        if (nav.enabled)
         {
-            CambiarEstado();
+            nav.CalculatePath(targetPosition, path);
+            Vector3[] allTheWaypoints = new Vector3[path.corners.Length + 2];
+            allTheWaypoints[0] = this.transform.position;
+            allTheWaypoints[allTheWaypoints.Length - 1] = targetPosition;
+
+            for (int i = 0; i < path.corners.Length; i++)
+            {
+                allTheWaypoints[i + 1] = path.corners[i];
+            }
+
+            float pathLength = 0;
+            for (int i = 0; i < allTheWaypoints.Length - 1; i++)
+            {
+                pathLength += Vector3.Distance(allTheWaypoints[i], allTheWaypoints[i + 1]);
+                if (DEBUG_DRAW)
+                {
+                    Debug.DrawLine(allTheWaypoints[i], allTheWaypoints[i + 1], Color.gray);
+                }
+            }
+
+            return pathLength;
         }
+        else
+        {
+            return 0;
+        }
+
     }
+
 }
