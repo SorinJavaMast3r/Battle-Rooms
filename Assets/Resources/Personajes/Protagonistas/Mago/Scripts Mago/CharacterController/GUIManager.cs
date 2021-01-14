177afecf30d7ff06ade12ed8e0647b4f514bd5ef
SorinJavaMast3r <45288233+SorinJavaMast3r@users.noreply.github.com>
2021-01-14 11:41:47 +0100

    [UPDATE] Arreglos en la parte estética del sistema de vida

--- Assets/Resources/Personajes/Protagonistas/Mago/Scripts Mago/CharacterController/GUIManager.cs
+++ Assets/Resources/Personajes/Protagonistas/Mago/Scripts Mago/CharacterController/GUIManager.cs
@@ -20,19 +20,19 @@ public class GUIManager : MonoBehaviour
         this.hpBar.value = (float)playerStats.currentHP / playerStats.maxHP;
         this.mpBar.value = (float)playerStats.currentMP / playerStats.maxMP;
 
-        this.hpText.text = ((float)playerStats.currentHP / playerStats.maxHP * 100).ToString() + "%";
-        this.mpText.text = ((float)playerStats.currentMP / playerStats.maxMP * 100).ToString() + "%";
+        this.hpText.text = playerStats.currentHP.ToString() + "/" + playerStats.maxHP;
+        this.mpText.text = playerStats.currentMP.ToString() + "/" + playerStats.maxMP;
     }
 
     public void updateHpBar(float currentHP, float maxHP) 
     {
         this.hpBar.value = currentHP / maxHP;
-        this.hpText.text = (currentHP / maxHP * 100).ToString() + "%";
+        this.hpText.text = currentHP.ToString() + "/" + maxHP;
     }
 
     public void updateMpBar(float currentMP, float maxMP)
     {
         this.mpBar.value = currentMP / maxMP;
-        this.mpText.text = (currentMP / maxMP * 100).ToString() + "%";
+        this.mpText.text = currentMP.ToString() + "/" + maxMP;
     }
 }
