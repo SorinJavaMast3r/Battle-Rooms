a38dd95c572490944302e026fb12422de0d620f1
AdrianPerezDiaz <adri672perez@gmail.com>
2021-01-18 13:31:04 +0100

    [ADD] GameOverPanel
    añadir panel a los controladores de los personajes

--- Assets/Resources/Personajes/Protagonistas/Mago/Scripts Mago/ManejoSkills/GestorSkills.cs
+++ Assets/Resources/Personajes/Protagonistas/Mago/Scripts Mago/ManejoSkills/GestorSkills.cs
@@ -34,63 +34,66 @@ public class GestorSkills : MonoBehaviour
         if (playerStats.dead)
             return;
 
-        if (Input.GetKeyDown(KeyCode.Alpha1) && this.animator.GetFloat("cooldown") <= 0 && playerStats.currentMP >= 7)
+        if (!AbrirMenu.instanciar.MenuPausaActivo && !Inventario.llamar.inventarioActivo)
         {
-            this.animator.Play("First Magic Attack");
-            
-            animationStartTime = Time.time + animationTime;
-            animationExit = true;
-            keyPressed = "q";
-
-            this.animator.SetFloat("cooldown", 200f);
-
-            playerStats.decreaseMP(7);
-        }
-        
-        this.animator.SetFloat("cooldown", this.animator.GetFloat("cooldown") - 1f);
-        
-        if (Input.GetKeyDown(KeyCode.Alpha2) && this.animator.GetFloat("cooldown") <= 0 && playerStats.currentMP >= 20)
-        {
-            this.animator.Play("Second Magic Attack");
-            animationStartTime = Time.time + animationTime;
-            animationExit = true;
-            keyPressed = "e";
-
-            this.animator.SetFloat("cooldown", 200f);
-
-            playerStats.decreaseMP(20);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha3) && this.animator.GetFloat("cooldown") <= 0 && playerStats.currentMP >= 15)
-        {
-            this.animator.Play("Third Magic Attack");
-            animationStartTime = Time.time + animationTime;
-            animationExit = true;
-            keyPressed = "r";
-
-            this.animator.SetFloat("cooldown", 200f);
-
-            playerStats.decreaseMP(15);
-        }
-
-        if (animationExit && Time.time > animationStartTime)
-        {
-            switch (keyPressed)
+            if (Input.GetKeyDown(KeyCode.Alpha1) && this.animator.GetFloat("cooldown") <= 0 && playerStats.currentMP >= 7)
             {
-                case "q":
-                    this.energyBall.lanzarHabilidad(this.gameObject);
-                    animationExit = false;
-                    break;
-                case "e":
-                    this.rayo.lanzarHabilidad(this.gameObject);
-                    animationExit = false;
-                    break;
-                case "r":
-                    this.earthSmash.lanzarHabilidad(this.gameObject);
-                    animationExit = false;
-                    break;
+                this.animator.Play("First Magic Attack");
+
+                animationStartTime = Time.time + animationTime;
+                animationExit = true;
+                keyPressed = "q";
+
+                this.animator.SetFloat("cooldown", 200f);
+
+                playerStats.decreaseMP(7);
             }
-        }
+
+            this.animator.SetFloat("cooldown", this.animator.GetFloat("cooldown") - 1f);
+
+            if (Input.GetKeyDown(KeyCode.Alpha2) && this.animator.GetFloat("cooldown") <= 0 && playerStats.currentMP >= 20)
+            {
+                this.animator.Play("Second Magic Attack");
+                animationStartTime = Time.time + animationTime;
+                animationExit = true;
+                keyPressed = "e";
+
+                this.animator.SetFloat("cooldown", 200f);
+
+                playerStats.decreaseMP(20);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha3) && this.animator.GetFloat("cooldown") <= 0 && playerStats.currentMP >= 15)
+            {
+                this.animator.Play("Third Magic Attack");
+                animationStartTime = Time.time + animationTime;
+                animationExit = true;
+                keyPressed = "r";
+
+                this.animator.SetFloat("cooldown", 200f);
+
+                playerStats.decreaseMP(15);
+            }
+
+            if (animationExit && Time.time > animationStartTime)
+            {
+                switch (keyPressed)
+                {
+                    case "q":
+                        this.energyBall.lanzarHabilidad(this.gameObject);
+                        animationExit = false;
+                        break;
+                    case "e":
+                        this.rayo.lanzarHabilidad(this.gameObject);
+                        animationExit = false;
+                        break;
+                    case "r":
+                        this.earthSmash.lanzarHabilidad(this.gameObject);
+                        animationExit = false;
+                        break;
+                }
+            }
+        }        
     }
 
 }
